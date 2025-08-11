83c1d1de2039713fe49a26096e7914d9920a0fc4
Manjunadhganta <gantamanjunadhreddy@gmail.com>
2025-08-11 22:15:39 +0530

    Added a feature in AiTools

--- src/components/AiTools.jsx
+++ src/components/AiTools.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { AiToolsData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
-import { useUser } from '@clerk/clerk-react'
+import { useClerk, useUser } from '@clerk/clerk-react'
 
 const AiTools = () => {
+    const { openSignIn } = useClerk();
     const navigate = useNavigate()
     const {user} = useUser()
+
+    const handleToolClick = (path) => {
+        if(user){
+            navigate(path);
+        } else{
+            openSignIn();
+        }
+    } 
   return (
     <div className='px-4 sm:px-20 xl:px-32 my-24'>
       <div className='text-center'>
@@ -15,7 +24,7 @@ const AiTools = () => {
 
       <div className='flex flex-wrap mt-10 justify-center'>
         {AiToolsData.map((tool, index)=> (
-            <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition-all duration-300 cursor-pointer' onClick={()=> user && navigate(tool.path)}>
+            <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition-all duration-300 cursor-pointer' onClick={()=> handleToolClick(tool.path)}>
                 <tool.Icon  className='w-12 h-12 p-3 text-white rounded-xl' style={{background : `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}}/>
                 <h3 className = 'mt-6 mb-3 text-lg font-semibold'>{tool.title}</h3>
                 <h3 className = 'text-gray-400 text-sm max-w-[95%]'>{tool.description}</h3>
